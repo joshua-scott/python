8e696f521ea27b9b61b7e79bd5035be5f0717c62
Joshua Scott <jscott313@gmail.com>
2017-08-03 13:24:01 +0300

    Completed ch4 tasks

--- ch4.py
+++ ch4.py
@@ -28,3 +28,34 @@ for i in range(limit):
     sum += i
 
 print("The sum was:", sum)
+
+
+# Task 4
+# Improve the calculator using a while-loop
+# It should only exit when the user commands, and have the option to change the inputted numbers
+
+print("Calculator")
+num1 = int(input("Give the first number: "))
+num2 = int(input("Give the second number: "))
+
+while True:
+    print("(1) +\n(2) -\n(3) *\n(4) /\n(5)Change numbers\n(6)Quit")
+    print("Current numbers:", num1, num2)
+    selection = int(input("Please select something (1-6): "))
+
+    if selection == 1:
+        print("The result is: " + str(num1 + num2))
+    elif selection == 2:
+        print("The result is: " + str(num1 - num2))
+    elif selection == 3:
+        print("The result is: " + str(num1 * num2))
+    elif selection == 4:
+        print("The result is: " + str(num1 / num2))
+    elif selection == 5:
+        num1 = int(input("Give the first number: "))
+        num2 = int(input("Give the second number: "))
+    elif selection == 6:
+        print("Thank you!")
+        break
+    else:
+        result = "Selection was not correct."
