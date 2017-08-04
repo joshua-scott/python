79445d10cc5b222d6648dda9446456d2ac61f8f2
Joshua Scott <jscott313@gmail.com>
2017-08-04 13:02:19 +0300

    3/4 ch6 exercises completed

---
+++ ch6.py
@@ -0,0 +1,43 @@
+# Task 1
+# Simple main() definition and subfunction call:
+
+# def main():
+#     print("Let's call the subfunction:")
+#     hello()
+#     print("Quitting.")
+
+# def hello():
+#     print("Hello there!")
+
+
+# Task 2
+# Using parameters
+
+# def main():
+#     poweroftwo(int(input("Give a number: ")))
+
+# def poweroftwo(power):
+#     print("The result is", 2**power)
+
+
+# Task 3
+# Default parameters
+
+# def main():
+#     while True:
+#         string = input("Write something (quit ends): ")
+#         if string == "quit": quit()
+#         elif len(string) < 10: tester()
+#         else: tester(string)
+
+# def tester(givenstring = "Too short"):
+#     print(givenstring)
+
+
+# Task 4
+# Return values
+
+
+# Call main if this file has been started as a program
+if __name__ == "__main__":
+    main()
