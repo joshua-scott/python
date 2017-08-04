88cd9a2c8105cd3436c393b2929051f6065b50f6
Joshua Scott <jscott313@gmail.com>
2017-08-04 14:37:39 +0300

    Added ch7 task 1

---
+++ ch7.py
@@ -0,0 +1,8 @@
+# Task 1
+# Import random module to simulate a coin flip
+from random import randint
+print("Heads!") if randint(0,1) == 1 else print("Tails!")
+
+# Task 2
+# Rock/paper/scissors
+from random import randint
