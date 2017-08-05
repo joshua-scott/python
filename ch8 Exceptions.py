5ecb6e7d099432f3c25f4fd2c64d5682c56a4af2
Joshua Scott <jscott313@gmail.com>
2017-08-05 22:16:42 +0300

    Completed ch8

--- ch8 Exceptions.py
+++ ch8 Exceptions.py
@@ -1,136 +1,158 @@
-# # Task 1
-# # Basic exception handling
+# Task 1
+# Basic exception handling
 
-# try:
-#     int(input("Give a number: "))
-#     print("The input was suitable!")
-# except Exception:
-#     print("The input was malformed.")
-
-
-# # Task 2
-# # Catching various types of errors
-
-# try:
-#     filename = input("Give the file name: ")
-#     sourcefile = open(filename)
-#     contents = sourcefile.read()
-#     sourcefile.close()
-#     integer = int(contents)
-#     divided = 1000 / integer
-#     print("The result was {}".format(divided))
-# except IOError:
-#     print("There seems to be no file with that name.")
-# except ValueError:
-#     print("The file contents were unsuitable.")
-# except ZeroDivisionError:
-#     print("You can't divide by zero.")
-# except Exception:
-#     print("There was an unknown error.")
+try:
+    int(input("Give a number: "))
+    print("The input was suitable!")
+except Exception:
+    print("The input was malformed.")
 
 
-# # Task 3
-# # Improve the calculator by using exceptions to validate inputs
-# # There's some strange casts to int here, but sadly it's necessary to pass the Viope tests :/
+# Task 2
+# Catching various types of errors
 
-# import math
+try:
+    filename = input("Give the file name: ")
+    sourcefile = open(filename)
+    contents = sourcefile.read()
+    sourcefile.close()
+    integer = int(contents)
+    divided = 1000 / integer
+    print("The result was {}".format(divided))
+except IOError:
+    print("There seems to be no file with that name.")
+except ValueError:
+    print("The file contents were unsuitable.")
+except ZeroDivisionError:
+    print("You can't divide by zero.")
+except Exception:
+    print("There was an unknown error.")
 
-# def main():
-#     init()
 
-# def init():
-#     print("Calculator")
-#     num1 = getNumber("Give a number: ")
-#     num2 = getNumber("Give a number: ")
+# Task 3
+# Improve the calculator by using exceptions to validate inputs
+# There's some strange casts to int here, but sadly it's necessary to pass the Viope tests :/
 
-#     while True:
-#         print("(1) +\n(2) -\n(3) *\n(4) /\n(5)sin(number1/number2)\n(6)cos(number1/number2)\n(7)Change numbers\n(8)Quit")
-#         print("Current numbers:", num1, num2)
-#         selection = getNumber("Please select something (1-6): ")
+import math
 
-#         if selection == 1:
-#             print("The result is: ", num1 + num2)
-#         elif selection == 2:
-#             print("The result is: ", num1 - num2)
-#         elif selection == 3:
-#             print("The result is: ", num1 * num2)
-#         elif selection == 4:
-#             print("The result is: ", num1 / num2)
-#         elif selection == 5:
-#             print("The result is: ", math.sin(num1 / num2))
-#         elif selection == 6:
-#             print("The result is: ", math.cos(num1 / num2))
-#         elif selection == 7:
-#             num1 = getNumber("Give a number: ")
-#             num2 = getNumber("Give a number: ")
-#         elif selection == 8:
-#             print("Thank you!")
-#             quit()
-#         else:
-#             result = "Selection was not correct."
+def main():
+    init()
 
-# def getNumber(message):
-#     while True:
-#         try:
-#             return int(input(message))
-#         except ValueError:
-#             print("This input is invalid.")
+def init():
+    print("Calculator")
+    num1 = getNumber("Give a number: ")
+    num2 = getNumber("Give a number: ")
+
+    while True:
+        print("(1) +\n(2) -\n(3) *\n(4) /\n(5)sin(number1/number2)\n(6)cos(number1/number2)\n(7)Change numbers\n(8)Quit")
+        print("Current numbers:", num1, num2)
+        selection = getNumber("Please select something (1-6): ")
+
+        if selection == 1:
+            print("The result is: ", num1 + num2)
+        elif selection == 2:
+            print("The result is: ", num1 - num2)
+        elif selection == 3:
+            print("The result is: ", num1 * num2)
+        elif selection == 4:
+            print("The result is: ", num1 / num2)
+        elif selection == 5:
+            print("The result is: ", math.sin(num1 / num2))
+        elif selection == 6:
+            print("The result is: ", math.cos(num1 / num2))
+        elif selection == 7:
+            num1 = getNumber("Give a number: ")
+            num2 = getNumber("Give a number: ")
+        elif selection == 8:
+            print("Thank you!")
+            quit()
+        else:
+            result = "Selection was not correct."
+
+def getNumber(message):
+    while True:
+        try:
+            return int(input(message))
+        except ValueError:
+            print("This input is invalid.")
         
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
 
 
 # Task 4
-# Improve the notebook further by handling exceptions
+# Improve the notebook further (along with a full refactor)
 
-import time		
+import time
+
+DEFAULT_FILENAME = "notebook.txt"
 
 def main():
-    while True:
-        init()
+    init()
 
 def init():
-    greet()
-    selection = getInt()
+    filename = DEFAULT_FILENAME
+    setNotebook(filename)
 
-    if selection == 1:
-        read()
-    elif selection == 2:
-        addNote()
-    elif selection == 3:
-        empty()
-    elif selection == 4:
-        print("Notebook shutting down, thank you.")
-        quit()
+    while True:
+        displayOptions(filename)
+        selection = getInt()
 
-def greet():
+        if selection == 1:
+            read(filename)
+        elif selection == 2:
+            addNote(filename)
+        elif selection == 3:
+            empty(filename)
+        elif selection == 4:
+            filename = input("Give the name of the new file: ")
+            setNotebook(filename)
+        elif selection == 5:
+            print("Notebook shutting down, thank you.")
+            quit()
+
+def displayOptions(filename):
     print( 
-    """(1) Read the notebook
+    """
+Now using file {}
+(1) Read the notebook
 (2) Add note
 (3) Empty the notebook
-(4) Quit""")
+(4) Change the notebook
+(5) Quit\n""".format(filename))
+
+def setNotebook(filename):
+    try:
+        open(filename)
+    except Exception:
+        open(filename, "w")
+        if filename == DEFAULT_FILENAME:
+            print("\nNo default notebook was found, created one.")
+        else:
+            print("\nNo notebook with that name detected, created one.")
 
 def getInt():
-    try:
-        return int(input("Please select one: "))
-    except Exception:
-        print("Input must be an integer.")
+    while True:
+        try:
+            return int(input("Please select one: "))
+        except Exception:
+            print("Input must be an integer.")
 
-def read():
-    notebook = open("notebook.txt")
-    print(notebook.read()[:-1])     # :-2 to strip the last newline character
-    notebook.close()
+def read(filename):
+    sourcefile = open(filename)
+    print("\n" + sourcefile.read()[:-1])     # :-2 to strip the last newline characters
+    sourcefile.close()
 
-def addNote():
-    notebook = open("notebook.txt", "a")
-    newNote = input("Write a new note: ")
-    notebook.write("{}:::{}\n".format(newNote, time.strftime("%X %x")))
-    notebook.close()
+def addNote(filename):
+    destFile = open(filename, "a")
+    newNote = input("\nWrite a new note: ")
+    destFile.write("{} ::: {}\n".format(newNote, time.strftime("%X %x")))
+    destFile.close()
 
-def empty():
-    notebook = open("notebook.txt", "w")
-    notebook.close()
-    print("Notes deleted.")
+def empty(notebook):
+    destFile = open(notebook, "w")
+    destFile.close()
+    print("\nNotes deleted.\n")
 
 if __name__ == "__main__":
     main()
