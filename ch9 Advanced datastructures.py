ef3ca5f56a634da865d41fc05de60e6df09ca6fd
Joshua Scott <jscott313@gmail.com>
2017-08-05 23:16:54 +0300

    Added ch9 task 1

---
+++ ch9 Advanced datastructures.py
@@ -0,0 +1,11 @@
+# Task 1
+# Basic lists
+myList = [ "Blue", "Red", "Yellow", "Green" ]
+print("The first item in the list is:", myList[0])
+print("The entire list printed one at a time:")
+for i in myList: print(i)
+
+
+# Task 2
+# Use lists to allow the user to: 
+# (1) add products, (2) remove items and (3) print the list and quit.
