44cb8be3a4ca9d5d2508934795a15501a8457b66
Joshua Scott <jscott313@gmail.com>
2017-08-06 20:42:57 +0300

    Completed final notebook project

---
+++ final project - notebook.py
@@ -0,0 +1,84 @@
+import pickle
+import time
+
+DEFAULT_FILENAME = "notebook.dat"
+
+def main():
+    init()
+
+def init():
+    notebook = setNotebook(DEFAULT_FILENAME)
+
+    while True:
+        displayOptions()
+        selection = getInt()
+        if selection == 1:   read(notebook)
+        elif selection == 2: add(notebook)
+        elif selection == 3: edit(notebook)
+        elif selection == 4: delete(notebook)
+        elif selection == 5: saveAndQuit(notebook, DEFAULT_FILENAME)
+
+def displayOptions():
+    print( 
+    """
+(1) Read the notebook
+(2) Add note
+(3) Edit a note
+(4) Delete a note
+(5) Save and quit\n""")
+
+def setNotebook(filename):
+    try:
+        return pickle.load(open(filename, "rb"))
+    except Exception:
+        with open(filename, "wb") as f:
+            pickle.dump([], f)
+            print("\nNo default notebook was found, created one.")
+            return []
+
+def getInt():
+    while True:
+        try:
+            return int(input("Please select one: "))
+        except Exception:
+            print("Input must be an integer.")
+
+def read(notebook):
+    for i in notebook: print(i)
+
+def add(notebook):
+    newNote = input("Write a new note: ")
+    notebook.append(formatNote(newNote))
+
+def edit(notebook):
+    print("The list has {} notes.".format(len(notebook)))
+    try:
+        index = int(input("Which of them will be changed?: "))
+        print(notebook[index])
+    except Exception:
+        print("Invalid index entered.")
+        return
+    newNote = input("Give the new note: ")
+    notebook[index] = formatNote(newNote)
+
+def delete(notebook):
+    print("The list has {} notes.".format(len(notebook)))
+    try:
+        index = int(input("Which of them will be deleted?: "))
+        deleted = notebook.pop(index)
+        print("Deleted note " + deleted)
+    except Exception:
+        print("Invalid index entered.")
+        return
+
+def formatNote(newNote):
+    return newNote.ljust(30) + "::: " + time.strftime("%X %x")
+
+def saveAndQuit(notebook, filename):
+    with open(filename, "wb") as f:
+            pickle.dump(notebook, f)
+    print("Notebook shutting down, thank you.")
+    quit()
+
+if __name__ == "__main__":
+    main()
