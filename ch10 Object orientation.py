908f654ab7b7da119edda2e1bdeb5fbe01ac17a4
Joshua Scott <jscott313@gmail.com>
2017-08-07 14:11:31 +0300

    Added ch10 task 3

--- ch10 Object orientation.py
+++ ch10 Object orientation.py
@@ -36,3 +36,29 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+# Task 3
+# Initialising an object's attributes using __init__
+
+class Player:
+    """Player-class: stores data on team colours and points."""
+    __points = 0
+    def __init__(self):
+        self.__teamColour = input("What colour do I get?: ")
+    def tellScore(self):
+        print("I am {}, we have {} points!".format(self.__teamColour, self.__points))
+    def goal(self):
+        self.__points += 1
+
+def main():
+    teamOne = Player()
+    teamTwo = Player()
+    teamOne.goal()
+    teamOne.goal()
+    teamTwo.goal()
+    teamOne.tellScore()     # Prints "I am Blue, we have 2 points!"
+    teamTwo.tellScore()     # Prints "I am Red, we have 1 points!"
+
+if __name__ == "__main__":
+    main()
