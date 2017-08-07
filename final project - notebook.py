6390b78e343a5c1661b4323c7f119e187f578510
Joshua Scott <jscott313@gmail.com>
2017-08-07 21:15:50 +0300

    Added docstrings and project summary

--- final project - notebook.py
+++ final project - notebook.py
@@ -1,3 +1,14 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                                                                   #
+#   This is the final project of the introductory Python course.    #
+#   It demonstrates the ability to use the fundamentals of Python   #
+#   to create a console-based simple notebook application.          #
+#   The application has the ability to read, add, edit, and delete  #
+#   notes, which are stored in a list. On exit, the list of notes   #
+#   is serialized to a file using the Pickle module.                #
+#                                                                   #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
 import pickle
 import time
 
@@ -7,6 +18,7 @@ def main():
     init()
 
 def init():
+    """The main logic of the program is contained here. Loops until user quits."""
     notebook = setNotebook(DEFAULT_FILENAME)
 
     while True:
@@ -28,6 +40,7 @@ def displayOptions():
 (5) Save and quit\n""")
 
 def setNotebook(filename):
+    """Reads the file using Pickle, and returns the data. Creates a file if it doesn't exist (and returns empty list)."""
     try:
         with open(filename, "rb") as source:
             return pickle.load(source)
@@ -38,6 +51,7 @@ def setNotebook(filename):
             return []
 
 def getInt():
+    """Prompts the user to input an int, and returns it. Loops until user gives acceptable input."""
     while True:
         try:
             return int(input("Please select one: "))
@@ -45,13 +59,16 @@ def getInt():
             print("Input must be an integer.")
 
 def read(notebook):
+    """Prints each note, one per line."""
     for i in notebook: print(i)
 
 def add(notebook):
+    """Gets input and appends it to the notebook. Calls formatNote() to add spacing and date."""
     newNote = input("Write a new note: ")
     notebook.append(formatNote(newNote))
 
 def edit(notebook):
+    """Allows user to edit a note (chosen by index)."""
     print("The list has {} notes.".format(len(notebook)))
     try:
         index = int(input("Which of them will be changed?: "))
@@ -63,6 +80,7 @@ def edit(notebook):
     notebook[index] = formatNote(newNote)
 
 def delete(notebook):
+    """Allows user to delete a note (chosen by index)."""
     print("The list has {} notes.".format(len(notebook)))
     try:
         index = int(input("Which of them will be deleted?: "))
@@ -73,9 +91,11 @@ def delete(notebook):
         return
 
 def formatNote(newNote):
-    return newNote.ljust(30) + "::: " + time.strftime("%X %x")
+    """Pads the given note to 30 chars (for neat printing), adds a visual separator, and the current time."""
+    return newNote.ljust(30) + "::: " + time.strftime("%X %x")  # Pad the note so they're always printed neatly to the terminal
 
 def saveAndQuit(notebook, filename):
+    """Uses Pickle to serialize the data to the given file, then quits."""
     with open(filename, "wb") as f:
             pickle.dump(notebook, f)
     print("Notebook shutting down, thank you.")
